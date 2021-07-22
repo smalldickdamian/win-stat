9f779c20fb56e711b730ba4357f255e5121794eb
Damian Freeman <38955757+Whishoryi@users.noreply.github.com>
2021-07-21 21:03:29 -0400

    done

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const os = require("os");
+
+const { name, version } = require("./package.json");
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.status(200).render(__dirname + "/views/home.ejs", {
+        os,
+        name,
+        version,
+        process
+    });
+});
+
+app.listen(10000, () => console.log("Listening."));
